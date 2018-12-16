8c54c268a343a834e7a95b826d156fd8d7a0340d
Clément Léger <clement@clement-leger.fr>
2018-12-16 16:35:29 +0100

    Switch to ArduinoJson 6

--- thermostat_control.ino
+++ thermostat_control.ino
@@ -34,7 +34,7 @@ Paint paint(image, 400, 300);    //width should be the multiple of 8
 #define INDOOR_TEMP     "io_2"
 #define INDOOR_HUMIDITY     "io_3"
 
-StaticJsonBuffer<400> jsonBuffer;
+StaticJsonDocument<200> jsonBuffer;
 HTTPClient http;
 
 void setup()
@@ -48,136 +48,138 @@ void setup()
 	}
 	Serial.println("Connected !");
 
-  if (epd.Init(EPD_PLL_100HZ, 2000000) != 0) {
-    Serial.println("e-Paper init failed");
-    while(1);
-  }
+	if (epd.Init(EPD_PLL_100HZ, 2000000) != 0) {
+		Serial.println("e-Paper init failed");
+		while(1);
+	}
 
-  /* This clears the SRAM of the e-paper display */
-  epd.ClearFrame();
-  epd.DisplayFrame();
-  Serial.println("Clear frame");
+	/* This clears the SRAM of the e-paper display */
+	epd.ClearFrame();
+	epd.DisplayFrame();
+	Serial.println("Clear frame");
 }
 
 int executeRequest(String request, String &ret)
 {
-	unsigned long timeout;
 	http.begin(CALAOS_API);
-  http.addHeader("Content-Type", "application/json");
+	http.addHeader("Content-Type", "application/json");
 	int httpCode = http.POST(request);
 	if(httpCode <= 0) {
-    Serial.print("HTTPS connection failed: ");
-    Serial.println(httpCode);
+		Serial.print("HTTPS connection failed: ");
+		Serial.println(httpCode);
 		http.end();
 		return 1;
 	}
 
 	String payload = http.getString();
-  ret = payload;
-  return 0;
+	ret = payload;
+	return 0;
 }
 
 int calaosGetState(String item, String &item_value)
 {
-  String output;
-  String payload;
+	String output;
+	String payload;
 
-	JsonObject& root = jsonBuffer.createObject();
+	JsonObject root = jsonBuffer.to<JsonObject>();
 	root["cn_user"] = CALAOS_USER;
 	root["cn_pass"] = CALAOS_PASSWORD;
 	root["action"] = "get_state";
 
-	JsonArray& data = root.createNestedArray("items");
+	JsonArray data = root.createNestedArray("items");
 	data.add(item);
 
-	root.printTo(output);
+	serializeJson(root, output);
 
 	int ret = executeRequest(output, payload);
 
-  JsonObject& resp = jsonBuffer.parseObject(payload);
-  http.end();
-  if (!resp.success()) {
-    Serial.println("parseObject() failed");
-    return 1;
-  }
+	DeserializationError error = deserializeJson(jsonBuffer, payload);
+	  
+	http.end();
+	if (error) {
+		Serial.println("deserializeJson() failed");
+		return 1;
+	}
+	JsonObject resp = jsonBuffer.as<JsonObject>();
+	item_value = resp[item].as<String>();
+	jsonBuffer.clear();
 
-  item_value = resp[item].as<String>();
-  jsonBuffer.clear();
-
-  return 0;
+	return 0;
 }
 
 int calaosSetState(String item, String item_value)
 {
-  String output;
-  String payload;
+	String output;
+	String payload;
 
-  JsonObject& root = jsonBuffer.createObject();
-  root["cn_user"] = CALAOS_USER;
-  root["cn_pass"] = CALAOS_PASSWORD;
-  root["action"] = "set_state";
-  root["id"] = item;
-  root["value"] = item_value;
+	JsonObject root = jsonBuffer.to<JsonObject>();
+	root["cn_user"] = CALAOS_USER;
+	root["cn_pass"] = CALAOS_PASSWORD;
+	root["action"] = "set_state";
+	root["id"] = item;
+	root["value"] = item_value;
 
-  root.printTo(output);
+	serializeJson(root, output);
 
-  int ret = executeRequest(output, payload);
+	int ret = executeRequest(output, payload);
 
-  JsonObject &resp = jsonBuffer.parseObject(payload);
-  http.end();
-  if (!resp.success()) {
-    Serial.println("parseObject() failed");
-    return 1;
-  }
-  bool status = (resp["success"] != "true");
-  jsonBuffer.clear();
- 
-  return status;
+	DeserializationError error = deserializeJson(jsonBuffer, payload);
+	
+	http.end();
+	if (error) {
+		Serial.println("deserializeJson() failed");
+		return 1;
+	}
+	JsonObject resp = jsonBuffer.as<JsonObject>();
+	bool status = (resp["success"] != "true");
+	jsonBuffer.clear();
+
+	return status;
 }
 
 int setTempSetpoint(float value)
 {
-   return calaosSetState(HEATER_SETPOINT, String(value, 0));
+	return calaosSetState(HEATER_SETPOINT, String(value, 0));
 }
 
 int getTempSetpoint(float *value)
 {
-  String setpoint;
-  int ret = calaosGetState(HEATER_SETPOINT, setpoint);
-  *value = setpoint.toFloat();
-  return ret;
+	String setpoint;
+	int ret = calaosGetState(HEATER_SETPOINT, setpoint);
+	*value = setpoint.toFloat();
+	return ret;
 }
 
 void loop()
 {
-  float v;
-  String out_temp = "0", out_humid = "0";
-  String in_temp = "0", in_humid = "0";
-  
-  calaosGetState(OUTDOOR_TEMP, out_temp);
-  calaosGetState(OUTDOOR_HUMIDITY, out_humid);
-  calaosGetState(INDOOR_TEMP, in_temp);
-  calaosGetState(INDOOR_HUMIDITY, in_humid);
+	float v;
+	String out_temp = "0", out_humid = "0";
+	String in_temp = "0", in_humid = "0";
 
-  getTempSetpoint(&v);
+	//~ calaosGetState(OUTDOOR_TEMP, out_temp);
+	//~ calaosGetState(OUTDOOR_HUMIDITY, out_humid);
+	calaosGetState(INDOOR_TEMP, in_temp);
+	calaosGetState(INDOOR_HUMIDITY, in_humid);
 
-  String s =  "Consigne: " + String(v, 0) + "C";
-  paint.Clear(UNCOLORED);
-  paint.DrawStringAt(0, 0, s.c_str(), &Font24, COLORED);
+	getTempSetpoint(&v);
 
-  s =  "Interieur Temp: " + in_temp + "C";
-  paint.DrawStringAt(0, 30, s.c_str(), &Font16, COLORED);
-  s =  "Interieur Humidite: " + in_humid + "%";
-  paint.DrawStringAt(0, 55, s.c_str(), &Font16, COLORED);
+	String s =  "Consigne: " + String(v, 0) + "C";
+	paint.Clear(UNCOLORED);
+	paint.DrawStringAt(0, 30, s.c_str(), &Font24, COLORED);
 
-  s =  "Exterieur Temp: " + out_temp + "C";
-  paint.DrawStringAt(0, 80, s.c_str(), &Font16, COLORED);
-  s =  "Exterieur Humidite: " + out_humid + "%";
-  paint.DrawStringAt(0, 105, s.c_str(), &Font16, COLORED);
-  
-  epd.SetPartialWindow(paint.GetImage(), 0, 0, paint.GetWidth(), paint.GetHeight());
-  epd.DisplayFrame();
-  Serial.println(v);
-  delay(20000);
+	s =  "Interieur Temp: " + in_temp + "C";
+	paint.DrawStringAt(0, 60, s.c_str(), &Font16, COLORED);
+	s =  "Interieur Humidite: " + in_humid + "%";
+	paint.DrawStringAt(0, 85, s.c_str(), &Font16, COLORED);
+
+	//~ s =  "Exterieur Temp: " + out_temp + "C";
+	//~ paint.DrawStringAt(0, 80, s.c_str(), &Font16, COLORED);
+	//~ s =  "Exterieur Humidite: " + out_humid + "%";
+	//~ paint.DrawStringAt(0, 105, s.c_str(), &Font16, COLORED);
+
+	epd.SetPartialWindow(paint.GetImage(), 0, 0, paint.GetWidth(), paint.GetHeight());
+	epd.DisplayFrame();
+	Serial.println(v);
+	delay(20000);
   
 }
